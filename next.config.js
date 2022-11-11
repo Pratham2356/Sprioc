b16c6fda842c327ee771623ecabc6a4ba6bbad16
Pratham2356 <jainpratham16@gmail.com>
2022-11-11 19:19:05 +0530

    setup

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "**",
+    //   },
+    // ],
+    domains: ["lh3.googleusercontent.com", "res.cloudinary.com"],
+  },
+};
